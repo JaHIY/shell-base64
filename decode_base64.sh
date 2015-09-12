13772b289ad9efb4f7eb2d446bf069304688ef00
JaHIY <jaklsy@gmail.com>
2015-09-12 20:04:45 +0800

    use symbolic link instead of real script

--- decode_base64.sh
+++ decode_base64.sh
@@ -1,7 +1,1 @@
-#!/bin/sh -
-
-WHO_AM_I="$(which "$0")"
-LIB_DIR="$(dirname "$(readlink -f "$WHO_AM_I")")/lib"
-. "${LIB_DIR}/base64.sh"
-
-decode_base64 "$@"
+encode_base64.sh
--- encode_base64.sh
+++ encode_base64.sh
@@ -1,7 +1,43 @@
 #!/bin/sh -
 
+readlink_canonicalize() (
+    cd "$(dirname "$1")"
+
+    local target_file="$(basename "$1")"
+
+    while [ -L "$target_file" ]; do
+        target_file="$(readlink "$target_file")"
+        cd "$(dirname "$target_file")"
+        target_file="$(basename "$target_file")"
+    done
+
+    local phys_dir="$(pwd -P)"
+    printf '%s\n' "${phys_dir}/${target_file}"
+)
+
 WHO_AM_I="$(which "$0")"
-LIB_DIR="$(dirname "$(readlink -f "$WHO_AM_I")")/lib"
+LIB_DIR="$(dirname "$(readlink_canonicalize "$WHO_AM_I")")/lib"
 . "${LIB_DIR}/base64.sh"
 
-encode_base64 "$@"
+BASENAME="$(basename "$0")"
+
+case "$BASENAME" in
+    'encode_base64'*)
+        encode_base64 "$@"
+        ;;
+    'strict_encode_base64'*)
+        strict_encode_base64 "$@"
+        ;;
+    'decode_base64'*)
+        decode_base64 "$@"
+        ;;
+    'urlsafe_encode_base64'*)
+        urlsafe_encode_base64 "$@"
+        ;;
+    'urlsafe_decode_base64'*)
+        urlsafe_decode_base64 "$@"
+        ;;
+    *)
+        printf '%s\n' 'Error: not matching a pattern' 1>&2
+        ;;
+esac
